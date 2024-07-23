f787ab238ca59b42f842ad38bb65fcb371116ba1
Ibrahim <muhammad.ibrahim@kwanso.com>
2024-07-23 16:26:28 +0500

    debugging

--- models/index.js
+++ models/index.js
@@ -6,14 +6,12 @@ const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(path.resolve(__dirname + "/../config/config.js"))[env];
+const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
-if(!config){
-  console.log(config)
-  throw new Error(`cannot read from: ${path.resolve(__dirname + "/../config/config.js")}`);
-} 
-  
+if(!config) throw new Error(`cannot read from: ${__dirname + "/../config/config.json"}`);
+
+console.log(path.resolve(__dirname + "/../config/config.json"))
 
 let sequelize;
 if (config.use_env_variable) {
