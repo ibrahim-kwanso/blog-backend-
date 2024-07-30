1b75e9f630c75db54c2eada388fe2113ef3a4606
Ibrahim <muhammad.ibrahim@kwanso.com>
2024-07-30 18:50:27 +0500

    update user logic changed

--- services/user.service.js
+++ services/user.service.js
@@ -1,17 +1,14 @@
-import User from "../models/user.js"
-import {StatusCodes as statusCodes} from "http-status-codes";
+import User from "../models/user.js";
+import { StatusCodes as statusCodes } from "http-status-codes";
 import ApiError from "../utils/apiError.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import {JWT_SECRET} from "../settings.js";
+import { JWT_SECRET } from "../settings.js";
 
 const createUserService = async (username, email, password) => {
   try {
     const user = await User.create({ username, email, password });
-    const token = jwt.sign(
-      { id: user.id, email: user.email },
-      JWT_SECRET
-    );
+    const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET);
     return token;
   } catch (error) {
     throw new ApiError(error.message, statusCodes.BAD_REQUEST);
@@ -45,18 +42,15 @@ const getAllUsersService = async () => {
 
 const updateUserService = async (id, updateData) => {
   try {
-    if(updateData.password){
-      updateData.password = await bcrypt.hash(updateData.password, 3);
+    const user = await User.findByPk(id);
+    if (!user) {
+      throw new ApiError("User Not Found", statusCodes.NOT_FOUND);
     }
-    const [updated] = await User.update(updateData, {
-      where: {
-        id: id,
-      },
-    });
 
-    if (updated) return getUserService(id);
+    user.set(updateData);
+    const updatedUser = await user.save({ individualHooks: true });
 
-    throw new ApiError("User Not Found", statusCodes.NOT_FOUND);
+    return updatedUser;
   } catch (error) {
     if (error instanceof ApiError) throw error;
     throw new ApiError(error.message, statusCodes.BAD_REQUEST);
@@ -66,13 +60,12 @@ const updateUserService = async (id, updateData) => {
 const deleteUserService = async (id) => {
   try {
     const deletedUser = await User.destroy({
-        where: {
-          id: id,
-        },
-      });
-  
-      if (deletedUser)
-        return deletedUser;
+      where: {
+        id: id,
+      },
+    });
+
+    if (deletedUser) return deletedUser;
 
     throw new ApiError("User Not Found", statusCodes.NOT_FOUND);
   } catch (error) {
@@ -83,32 +76,27 @@ const deleteUserService = async (id) => {
 
 const signupService = async (email, password) => {
   try {
-    const user = await User.scope('withPassword').findOne({
+    const user = await User.scope("withPassword").findOne({
       where: {
-        email: email
+        email: email,
       },
     });
 
-    if (!user)
-      throw new ApiError("Invalid Credentials", statusCodes.NOT_FOUND);
+    if (!user) throw new ApiError("Invalid Credentials", statusCodes.NOT_FOUND);
 
     const passwordMatch = await bcrypt.compare(password, user.password);
 
     if (!passwordMatch)
       throw new ApiError("Invalid Credentials", statusCodes.NOT_FOUND);
 
-    const token = jwt.sign(
-      { id: user.id, email: user.email },
-      JWT_SECRET
-    );
+    const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET);
 
     return token;
-
   } catch (error) {
     if (error instanceof ApiError) throw error;
     throw new ApiError(error.message, statusCodes.BAD_REQUEST);
   }
-}
+};
 
 export {
   createUserService,
@@ -116,5 +104,5 @@ export {
   getAllUsersService,
   updateUserService,
   deleteUserService,
-  signupService
+  signupService,
 };
